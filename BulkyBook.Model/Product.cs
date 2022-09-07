98fb8c931c6ac490b85205a7a5c31de4aef80fd2
mohammadmhosseini <moh79hosseini@gmail.com>
2022-09-07 12:30:04 +0430

    End Product CRUD Section

--- BulkyBook.Model/Product.cs
+++ BulkyBook.Model/Product.cs
@@ -25,31 +25,37 @@ namespace BulkyBook.Model
         public string Author { get; set; }
 
         [Required]
-        [Range(1,10000)]
+        [Range(1,500000)]
+        [Display(Name = "لیست قیمت")]
         public double ListPrice { get; set; }
 
         [Required]
-        [Range(1, 10000)]
+        [Range(1, 500000)]
+        [Display(Name = "قیمت برای 1 تا 50 عدد")]
         public double Price { get; set; }
 
         [Required]
-        [Range(1, 10000)]
+        [Range(1, 500000)]
+        [Display(Name = "قیمت برای 51 تا 100 عدد")]
         public double Price50 { get; set; }
 
         [Required]
-        [Range(1, 10000)]
+        [Range(1, 500000)]
+        [Display(Name = "قیمت برای بیشتر از 100 عدد")]
         public double Price100 { get; set; }
 
         [ValidateNever]
         public string ImageUrl { get; set; }
 
         [Required]
+        [Display(Name = "دسته بندی")]
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")] // no need
         [ValidateNever]
         public Category Category { get; set; }
 
         [Required]
+        [Display(Name = "نوع پوشش")]
         public int CoverTypeId { get; set; }
         [ValidateNever]
         public CoverType CoverType { get; set; }
